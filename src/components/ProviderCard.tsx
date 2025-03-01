41b1726cff6be15f84caabe445074fb9c7b79185
nauh_pv <vanhuana4k56@gmail.com>
2025-03-01 14:11:55 +0700

    fix color header

--- src/components/ProviderCard.tsx
+++ src/components/ProviderCard.tsx
@@ -17,8 +17,12 @@ const ProviderCard = ({ name, totalGame, logo }: ProviderCardProps) => {
         className="w-full h-auto object-cover"
       />
       <div className="absolute w-full h-1/2 bg-[#F5F9FF0D]/5 bottom-0 left-0 flex flex-col justify-center items-center rounded-b-[8px] gap-2 group-hover:hidden duration-200 transition ease-in-out delay-200">
-        <p className="underline text-[13px] leading-[18.2px] text-white">{name}</p>
-        <p className="underline text-[11px] leading-[11px] text-secondWhite">{totalGame} game</p>
+        <p className="underline text-[13px] leading-[18.2px] text-white">
+          {name}
+        </p>
+        <p className="underline text-[11px] leading-[11px] text-secondWhite">
+          {totalGame} game
+        </p>
       </div>
     </div>
   );
