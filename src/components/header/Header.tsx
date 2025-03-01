fae7ebfdd55a1feb0e412e9c8fac43913c6c1d25
nauh_pv <vanhuana4k56@gmail.com>
2025-03-01 13:35:43 +0700

    fix hover button and color

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -12,25 +12,31 @@ const Header = () => {
     <header className="py-[16px] fixed z-50 w-full h-fit flex items-center justify-between px-[240px]">
       <div className="flex items-center gap-[57px]">
         <div className="gap-[6px] flex items-center">
-          <HiOutlineBars3CenterLeft size={iconSizes.large} />
-          <Image
-            src="/images/White_logo.png"
-            alt="Logo white"
-            width={100}
-            height={100}
-            className="h-[30px] w-[53px]"
-          />
+          <button>
+            <HiOutlineBars3CenterLeft size={iconSizes.large} />
+          </button>
+          <a href="/">
+            <Image
+              src="/images/White_logo.png"
+              alt="Logo white"
+              width={100}
+              height={100}
+              className="h-[30px] w-[53px]"
+            />
+          </a>
         </div>
         <div className="flex items-center gap-[32px]">
-          <CiSearch size={iconSizes.large} />
+          <button className="hover:text-primaryBlue transition duration-200 text-white">
+            <CiSearch size={iconSizes.large} />
+          </button>
           <MenuHeader />
         </div>
       </div>
       <div className="gap-[8px] flex items-center">
-        <button className="py-[9.3px] px-[12px] text-[14px] leading-[15.4px] rounded-[10px] bg-secondAzure text-white tracking-[0.42px]">
+        <button className="py-[9.3px] px-[12px] text-[14px] leading-[15.4px] rounded-[10px] bg-secondAzure text-white tracking-[0.42px] hover:bg-secondAzure/80 transition duration-200">
           LOGIN
         </button>
-        <button className="py-[9.3px] px-[12px] text-[14px] rounded-[10px] bg-primaryGreen text-white leading-[15.4px] tracking-[0.42px]">
+        <button className="py-[9.3px] px-[12px] text-[14px] rounded-[10px] bg-primaryGreen text-white leading-[15.4px] tracking-[0.42px] hover:bg-primaryGreen/90 transition duration-200">
           REGISTRATION
         </button>
       </div>
